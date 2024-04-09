1f9bef7b72ba72cf01b10d9202802433715cf5a9
SashveerRamjathan <sashveerlakhan0@gmail.com>
2024-04-09 23:55:02 +0200

    Created Recipe Step Class

---
+++ RecipeStep.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10361554_PROG6221_POE_Part1
+{
+    internal class RecipeStep
+    {
+        public string? StepDescription { get; set; }
+    }
+}
