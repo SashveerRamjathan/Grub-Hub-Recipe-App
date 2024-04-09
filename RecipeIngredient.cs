d3b98c7c6aaad31ed061f8c27767e6a9b0ae7a61
SashveerRamjathan <sashveerlakhan0@gmail.com>
2024-04-09 23:53:16 +0200

    Created Recipe Ingredient Class

---
+++ RecipeIngredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10361554_PROG6221_POE_Part1
+{
+    internal class RecipeIngredient
+    {
+        public string? IngredientName { get; set; }
+        public double IngredientQuantity { get; set; }
+        public string? UnitOfMeasurement { get; set; }
+    }
+}
