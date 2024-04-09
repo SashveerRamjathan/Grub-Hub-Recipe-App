ef77e92a6dd3909c85804a2f9bb5b08d9c6a94a3
SashveerRamjathan <sashveerlakhan0@gmail.com>
2024-04-09 23:56:15 +0200

    Created Recipe Class

---
+++ Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10361554_PROG6221_POE_Part1
+{
+    internal class Recipe
+    {
+        public string? RecipeName { get; set; }
+        public int NumberOfIngredients { get; set; }
+        public int NumberOfSteps { get; set; }
+
+        public ArrayList Ingredients = new ArrayList();
+
+        public ArrayList Steps = new ArrayList();
+    }
+}
