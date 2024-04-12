4544f53d0816a8f9875fc232b10618ab8d4855b8
SashveerRamjathan <sashveerlakhan0@gmail.com>
2024-04-12 23:55:37 +0200

    Added comments to RecipeIngredient class.

--- RecipeIngredient.cs
+++ RecipeIngredient.cs
@@ -8,8 +8,8 @@ namespace ST10361554_PROG6221_POE_Part1
 {
     internal class RecipeIngredient
     {
-        public string? IngredientName { get; set; }
-        public double IngredientQuantity { get; set; }
-        public string? UnitOfMeasurement { get; set; }
+        public string? IngredientName { get; set; } // stores the ingredient name
+        public double IngredientQuantity { get; set; } // stores the quantity of the ingredient added
+        public string? UnitOfMeasurement { get; set; } // stores the unit of measurement of the ingredient e.g. cup
     }
 }
